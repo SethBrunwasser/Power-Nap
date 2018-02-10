64236b0bd50dd93e7371dc36225cd84eb4f7f474
Seth Brunwasser <sbrunwasser1998@gmail.com>
2018-02-10 14:05:20 -0700

    No faces detected
    Turns off display in the event no faces are detected.

--- main.py
+++ main.py
@@ -36,19 +36,27 @@ if "__main__" == __name__:
 		if counter % 2 == 0:
 			ret, frame = video_capture.read()
 			if frame is not None:
-					recognized_face, label = predict(face_recognizer, subjects, frame)
+					recognized_face, labels, faces = predict(face_recognizer, subjects, frame)
+					print(labels)
+					print(faces)
 					cv2.imshow('Face Recognizer', recognized_face)
 					
-					# If face is unknown, turn off display
-					if label:
-						if label == -1:
-							# Uses 5 frame buffer to have smoother transitions between on/off display
-							label_history.append(label)
-							if len(label_history) == 20 and all(label == -1 for label in label_history):
-								turnoff()
-								label_history = []
-						else:
-							turnon()
+					#if labels:
+
+						# Update face data every 300 frames
+						#if counter % 300 == 0 and len(labels) == 1 and labels[0] != -1:
+						#	update(labels, faces)
+
+					# If face is unknown or no faces detected, turn off display
+					if labels is None or -1 in labels:
+						# Uses 5 frame buffer to have smoother transitions between on/off display
+						label_history.append(-1)
+						if len(label_history) == 10 and all(label == -1 for label in label_history):
+							turnoff()
+							label_history = []
+					else:
+						turnon()
+						label_history = []
 
 					if cv2.waitKey(1) & 0xFF == ord('q'):
 						break
