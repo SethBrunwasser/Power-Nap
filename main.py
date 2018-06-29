e322f2af383ca9b5a2951c64bd04aada119ed8a9
Seth Brunwasser <sbrunwasser1998@gmail.com>
2018-06-29 15:14:33 -0700

    Update stream

--- main.py
+++ main.py
@@ -4,9 +4,9 @@
 # Press Q to exit
 
 from recognizer import Recognizer
-from viewer import Viewer
 from database import UsersDB
 from flask import Flask, render_template, Response, make_response
+from multiprocessing import Process
 import time
 import threading
 import cv2
@@ -23,8 +23,7 @@ db.__del__()
 subjects = dict(subjects)
 
 recognizer = Recognizer()
-viewer = Viewer(recognizer, subjects)
-viewer.start()
+#viewer.start()
 
 app = Flask(__name__)
 
@@ -33,43 +32,58 @@ def index():
 	return render_template('index.html')
 
 def stream():
-	print("streaming")
 
 	frame = None
 	global_frame = None
+	prev = None
+	counter = 0
+	from viewer import processFrame as pf
+	print("streaming")
+
+	
 	while True:
 		try:
 			
+
 			#_, img = viewer.video_capture.read()
-			
-			img = viewer.counter
-			print(img)
+			#threading.lock.acquire()
+			#img = viewer.currentFrame
+			#threading.lock.release()
+			#print(img)
 
 			
 			#cv2.imshow('Image before broadcast', img)
+			img = pf(recognizer, counter, subjects)
 
-			#ret, jpeg = cv2.imencode('.jpg', img)
-			#frame = jpeg.tobytes()
-
+			#print(img)
+			counter += 1
+			#print("Counter:\t" + str(counter))
+			#print("Img:\t\t" + str(img))
+			if img is not None:
+				ret, jpeg = cv2.imencode('.jpg', img)
+				prev = jpeg
+				frame = jpeg.tobytes()
+			else:
+				frame = prev.tobytes()
+			
 		except Exception as e:
 			raise e
-		'''finally:
+		finally:
 			if frame != None:
 				global_frame = frame
-				viewer.cancel()
 				yield (b'--frame\r\n'
 					b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
-
 			else:
 				yield (b'--frame\r\n'
-	                            b'Content-Type: image/jpeg\r\n\r\n' + global_frame + b'\r\n\r\n')'''
+	                            b'Content-Type: image/jpeg\r\n\r\n' + global_frame + b'\r\n\r\n')
 		
-
 
 @app.route('/video_feed')
 def video_feed():
 	return Response(stream(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
 if "__main__" == __name__:
-
-	app.run(debug=True, threaded=True)
+	p = Process(target=stream)
+	p.start()
+	app.run(debug=True, use_reloader=False)
+	p.join()
--- viewer.py
+++ viewer.py
@@ -6,71 +6,49 @@ from time import sleep
 from recognizer import Recognizer
 from screen import turnoff, turnon
 
-class Viewer(Thread):
-	"""docstring for Viewer"""
-	def __init__(self, recognizer, subjects):
-		super(Viewer, self).__init__()
-		self.recognizer = recognizer
-		self.video_capture = cv2.VideoCapture(0)
-		self.subjects = subjects
-		self.label_history = []
-		self.counter = 0
-		self.labels = []
+
+video_capture = cv2.VideoCapture(0)
+#subjects = subjects
+label_history = []
+#counter = 0
+labels = []
 
 
-		# Status variables
-		self.daemon = False
-		self.currentFrame = None
-		self.isRunning = False
-		self.screen = True
-		self.unauthorizedDetected = False
+# Status variables
+daemon = False
+currentFrame = None
+isRunning = False
+screen = True
+unauthorizedDetected = False
 
 
 
-	def run(self):
-		''' Threading to allow for frame processing in parallel with other functions '''
-		
-		self.isRunning = True
-		while self.isRunning:
-			self.currentFrame = self.processFrame()
-			self.counter += 1
-			#print(self.currentFrame)
-			sleep(1)
-		print("Exited.")
+def processFrame(recognizer, counter, subjects):
+  # Only process every other frame to reduce CPU usage
+  global label_history
+  global labels
+  if counter % 2 == 0:
+    ret, frame = video_capture.read()
+    if frame is not None:
+        recognized_face, labels, faces = recognizer.predict(subjects, frame)
+        returnFrame = recognized_face
 
-
-	def processFrame(self):
-		# Only process every other frame to reduce CPU usage
-		if self.counter % 2 == 0:
-			ret, frame = self.video_capture.read()
-			if frame is not None:
-					recognized_face, self.labels, faces = self.recognizer.predict(self.subjects, frame)
-					returnFrame = recognized_face[:]
-					cv2.imshow('Face Recognizer', recognized_face)
-
-					# If face is unknown or no faces detected, turn off display
-					if self.labels is None or -1 in self.labels:
-						# Uses 5 frame buffer to have smoother transitions between on/off display
-						self.label_history.append(-1)
-						if len(self.label_history) == 10 and all(label == -1 for label in self.label_history):
-							turnoff()
-							self.screen = False
-							self.label_history = []
-							self.unauthorizedDetected = True
-					else:
-						turnon()
-						self.screen = True
-						self.label_history = []
-						self.unauthorizedDetected = False
-						# Update face data every 300 frames
-						#if self.counter % 25 == 0 and len(self.labels) == 1:
-							#self.recognizer.update(faces, self.labels)
-					return returnFrame
-
-	def cancel(self):
-		''' End the thread '''
-		self.isRunning = False
-
-	
-	#def setCurrentFrame(self, value):
-#
+        # If face is unknown or no faces detected, turn off display
+        if labels is None or -1 in labels:
+          # Uses 5 frame buffer to have smoother transitions between on/off display
+          label_history.append(-1)
+          if len(label_history) == 10 and all(label == -1 for label in label_history):
+            turnoff()
+            screen = False
+            label_history = []
+            unauthorizedDetected = True
+        else:
+          turnon()
+          screen = True
+          label_history = []
+          unauthorizedDetected = False
+          # Update face data every 300 frames
+          #if self.counter % 25 == 0 and len(self.labels) == 1:
+            #self.recognizer.update(faces, self.labels)
+        return returnFrame
+    return "Borked"
